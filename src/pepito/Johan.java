13117de389ef8daea80096a3de26a9f57224f541
Johan Rojas <johan.rs@hotmail.com>
2011-10-07 20:16:58 -0600

    Agregue nueva clase con nombre Johan

---
+++ src/pepito/Johan.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pepito;
+
+/**
+ *
+ * @author ITSPS
+ */
+public class Johan {
+    
+}
